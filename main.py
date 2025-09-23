38bd9296c29e9e2635bec263a980348a760f1942
johannmaxime <johannmaxime@yahoo.fr>
2025-09-23 14:42:20 +0100

    auth jwt

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from contextlib import asynccontextmanager
 from asgi_correlation_id import CorrelationIdMiddleware
 from fastapi import FastAPI, HTTPException
 from fastapi.exception_handlers import http_exception_handler
+
 from database import database
 from logging_conf import configure_logging
 from routers.post import router as post_router
+from routers.user import router as user_router
 
 logger = logging.getLogger(__name__)
 
@@ -20,13 +22,11 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(CorrelationIdMiddleware)
 
-
 app.include_router(post_router)
+app.include_router(user_router)
 
 
 @app.exception_handler(HTTPException)
 async def http_exception_handle_logging(request, exc):
     logger.error(f"HTTPException: {exc.status_code} {exc.detail}")
     return await http_exception_handler(request, exc)
-
-
